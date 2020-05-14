a92e8f0c666bc2622c3ec3554b1c9c4c5bd48566
babs12316 <44439609+babs12316@users.noreply.github.com>
2020-05-14 15:12:31 +0200

    css and loader change

--- src/components/RssFeedFrom/RssFeedForm.js
+++ src/components/RssFeedFrom/RssFeedForm.js
@@ -1,12 +1,14 @@
 import React, { useState, useRef } from "react";
 import Pagination from "../Pagination/Pagination";
-import './RssFeedForm.css';
+import "./RssFeedForm.css";
 const RssFeedForm = () => {
   //Get feedData
   const [feedData, setFeedData] = useState(null);
-//  
+  // sets error message if any
   const [error, setError] = useState(null);
-  
+  // shows loading icon while waiting for feedback from Rss feed
+  const [loading, setLoading] = useState(false);
+
   // RSS parser NPM module
   let Parser = require("rss-parser");
   let parser = new Parser();
@@ -21,20 +23,24 @@ const RssFeedForm = () => {
   };
 
   const fetchRssFeedData = async () => {
-  //empty error for each fresh call
-  setError(null);
-  
+    //empty error for each fresh call
+    setError(null);
+    //empty feedData for each fresh call
+    setFeedData(null);
+    // start loading
+    setLoading(true);
     // await make sure next line of code is not exccuted till we get parsed RSS feed value
-    try{
-       const feed = await parser.parseURL(inputEl.current.value);
-        setFeedData(feed);
-    }catch(err){
-          setError(err.message)
+    try {
+      const feed = await parser.parseURL(inputEl.current.value);
+      setFeedData(feed);
+      //stop loading
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
     }
   };
 
-
-//https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml
+  //https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml
 
   return (
     <React.Fragment>
@@ -44,8 +50,10 @@ const RssFeedForm = () => {
         <br />
         <button onClick={handClick}>Submit</button>
       </form>
+      {loading && <div id="loader"></div>}
+
       <Pagination feedData={feedData}></Pagination>
-     <div id="error">{error}</div>
+      <div id="error">{error}</div>
     </React.Fragment>
   );
 };
